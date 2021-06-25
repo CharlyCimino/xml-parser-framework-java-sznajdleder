2018b93e2df894feb6adff53dafa5b59563deeb9
Charly Cimino <carlosecimino@gmail.com>
2021-06-25 11:58:28 -0300

    Proyecto creado

---
+++ src/xmlparserframework/Test.java
@@ -0,0 +1,15 @@
+package xmlparserframework;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
